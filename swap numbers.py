5d1b19492dddfd11afecb2c481243d00d883bb40
rohith <rohith26.rk@gmail.com>
2023-09-19 13:44:22 +0530

    Merge remote-tracking branch 'origin/py-project' into py-project
    # Conflicts:
    #	swap numbers.py

--- swap numbers.py
+++ swap numbers.py
@@ -1,8 +1,10 @@
-a = input("enter value of a = ")
-b = input("enter value of b =")
-c = a
-a = b
-b = c
+#
 
-print ("a = " + a)
-print("b = " + b)
+var_1 = 123
+var_2 = 10
+var_3 = var_1 > var_2
+print(var_3)
+print(type(var_3))
+print(var_1)
+print(var_2)
+print(var_1 + var_2)
